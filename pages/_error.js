11996e4bb51cfe5696804014b8e9c3be30d7b64e
ABHISHEK BEEGUN <abhishekbeegun0642@gmail.com>
2022-11-13 06:03:06 -0800

    eslint errorfixed

--- pages/_error.js
+++ pages/_error.js
@@ -4,7 +4,7 @@ function ERROR() {
   return (
     <>
     <div className="h-[100vh] w-full flex items-center justify-center bg-black">
-     <iframe src='https://my.spline.design/head-97a38629d56e4efb933fea77fcf4f820/' frameborder='0' width='2900px' height='100%'></iframe>  
+     <iframe src='https://my.spline.design/head-97a38629d56e4efb933fea77fcf4f820/' frameBorder='0' width='2900px' height='100%'></iframe>  
     </div>
     </>
 
